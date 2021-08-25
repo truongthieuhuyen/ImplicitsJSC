709f804c3b879bf985bb43c3b6539d1ad82aecdc
truongthieuhuyen <nguyentientruong97@gmail.com>
2021-08-25 17:34:52 +0700

    home page(100%) company page(100%)

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,30 +1,30 @@
 console.log("Running javascript")
 
 $(document).ready(function(){
-        // Initializes the carousel
-            $(".start-slide").click(function(){
-            	$("#myCarousel").carousel('cycle');
-            });
-        // Stops the carousel
-            $(".pause-slide").click(function(){
-            	$("#myCarousel").carousel('pause');
-            });
-
-        $(".carousel-control-prev").click(function(){
-                $("#myCarousel").carousel('prev');
-        });
-        $(".carousel-control-next").click(function(){
-                $("#myCarousel").carousel('next');
-        });
-
-        // Cycles the carousel to a particular frame
-            $(".slide-one").click(function(){
-            	$("#myCarousel").carousel(0);
-            });
-            $(".slide-two").click(function(){
-            	$("#myCarousel").carousel(1);
-            });
-            $(".slide-three").click(function(){
-            	$("#myCarousel").carousel(2);
-            });
+//        // Initializes the carousel
+//            $(".start-slide").click(function(){
+//            	$("#myCarousel").carousel('cycle');
+//            });
+//        // Stops the carousel
+//            $(".pause-slide").click(function(){
+//            	$("#myCarousel").carousel('pause');
+//            });
+//
+//        $(".carousel-control-prev").click(function(){
+//                $("#myCarousel").carousel('prev');
+//        });
+//        $(".carousel-control-next").click(function(){
+//                $("#myCarousel").carousel('next');
+//        });
+//
+//        // Cycles the carousel to a particular frame
+//            $(".slide-one").click(function(){
+//            	$("#myCarousel").carousel(0);
+//            });
+//            $(".slide-two").click(function(){
+//            	$("#myCarousel").carousel(1);
+//            });
+//            $(".slide-three").click(function(){
+//            	$("#myCarousel").carousel(2);
+//            });
 });
